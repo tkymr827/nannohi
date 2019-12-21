a561f7dcdfb6052cae1db725ed4033a7224c485f
tkym <lelele5@RyoyanoMacBook-Air.local>
2019-12-21 11:28:31 +0900

    docs doko

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <h1>{{content}}</h1>
+      <h2>{{month}}月{{day}}日</h2>
+      <div class="form">
+        <input type="text" v-model="month"><p>月</p>
+        <input type="text" v-model="day"><p>日</p>
+      </div>
+
+      <button @click="check">見る</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      month: '',
+      day: '',
+      content: ''
+    }
+  },
+  methods: {
+    check: function () {
+    //   this.url = 'http://numbersapi.com/' + this.month + '/' + this.day + '/date'
+      axios.get('http://numbersapi.com/' + this.month + '/' + this.day + '/date')
+        .then(response => {
+          this.content = response.data
+        })
+      axios.get('https://script.google.com/macros/s/AKfycbyG2TWL7bSBPJ0ZEeCBcZstZP2yTShdo72OADZgsS3FawpsbUs/exec?text=' + this.content + '&source=en&target=jp')
+        .then(response => {
+          console.log(response)
+        })
+    }
   }
+
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+    padding:0;
+    margin:0;
+    box-sizing: border-box;
 }
+html{
+    font-size:62.5%;
+}
+#app {
+    background:#3c4c9c ;
+    color: #c4b66a;
+    min-height:100vh;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    .form{
+        display:flex;
+
+    }
+}
+
 </style>
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  publicPath: '/weathernow/',
+  assetsDir: './',
+  outputDir: 'docs',
+  filenameHashing: false,
+  productionSourceMap: false
+}
